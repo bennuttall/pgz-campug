87c30420ae91159e804038a75e8a0abcd8657e9e
Ben Nuttall <ben@bennuttall.com>
2018-11-15 22:56:44 +0000

    Improve jumping logic

--- game.py
+++ game.py
@@ -3,19 +3,22 @@ HEIGHT = 384
 
 alien = Actor('alien')
 alien.pos = (50, 320)
+alien.jumping = False
 
 def draw():
     screen.clear()
     alien.draw()
 
 def update():
-    if keyboard[keys.UP]:
-        if alien.y > 200:
-            alien.y -= 2
+    if alien.y == 320 and keyboard[keys.UP] or alien.jumping:
+        if alien.y > 100:
+            alien.y -= 10
+            alien.jumping = True
+        else:
+            alien.jumping = False
     elif alien.y < 320:
-        alien.y += 3
+        alien.y += 10
     if keyboard[keys.LEFT] and alien.left > 0:
-        alien.x -= 2
+        alien.x -= 5
     if keyboard[keys.RIGHT] and alien.right < WIDTH:
-        alien.x += 2
-    
+        alien.x += 5
